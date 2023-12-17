8189dc6eac15c5b8b58fb539e720484173ce2c3f
Keerthi K <keerthireddyk33@gmail.com>
2023-12-17 20:23:04 +0530

    mysql.sh

---
+++ mysql.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MongoDB_Host = mongodb.aiawsdevops.online
+
+echo -e "Script started executed at $Y $TIMESTAMP $N" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo  -e "Error:: $2 ... $R FAILED $N"
+        exit 1 #if error arises you can't proceed, give other than zero
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R Error:: Please run the script with root user $N"
+    exit 1 #if error arises you can't proceed, give other than zero
+else
+    echo -e "You are $G root $N user"
+fi
+
+dnf module disable mysql -y
+
+VALIDATE $? "Disabling current version of mysql"
+
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
+VALIDATE $? "Copying mysql repo"
+
+dnf install mysql-community-server -y
+
+VALIDATE $? "Installing MYSQL server"
+
+systemctl enable mysqld
+
+VALIDATE $? "Enable Mysql"
+
+systemctl start mysqld
+
+VALIDATE $? "Start mysql"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+
+VALIDATE $? "setting up mysql root password"
+
+mysql -uroot -pRoboShop@1
+
+VALIDATE $? "Checking the new password"
+
+#check netstat -lnpt
+#check sudo less /var/log/messages
+
