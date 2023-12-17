d88ad40b314ef69890f1ae90d897db9c45653319
Keerthi K <keerthireddyk33@gmail.com>
2023-12-17 17:39:36 +0530

    catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -29,67 +29,67 @@ else
     echo -e "You are $G root $N user"
 fi
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>> $LOGFILE
 
 VALIDATE $? "Disabling current version of nodejs"
 
-dnf module enable nodejs:18 -y
+dnf module enable nodejs:18 -y &>> $LOGFILE
 
 VALIDATE $? "Enabling Nodejs 18"
 
-dnf install nodejs -y
+dnf install nodejs -y &>> $LOGFILE
 
 VALIDATE $? "Installing Nodejs 18"
 
-useradd roboshop 
+useradd roboshop &>> $LOGFILE
 
 VALIDATE $? "Creating user roboshop"
 
-mkdir /app
+mkdir /app &>> $LOGFILE
 
 VALIDATE $? "Creating app directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
 
 VALIDATE $? "Downloading Catalogue application"
 
-cd /app 
+cd /app &>> $LOGFILE
 
 VALIDATE $? "Entering into app directory"
 
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>> $LOGFILE
 
 VALIDATE $? "Unzipping the catalogue application"
 
-npm install 
+npm install &>> $LOGFILE
 
 VALIDATE $? "Installing Dependencies"
 
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service # Currently, we are in app directory but catalogue.service app was downloaded in roboshop-shell so give absolute path
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE# Currently, we are in app directory but catalogue.service app was downloaded in roboshop-shell so give absolute path
 
 VALIDATE $? "Copying catalogue service file"
 
-systemctl daemon-reload
+systemctl daemon-reload &>> $LOGFILE
 
 VALIDATE $? "Catalogue Daemon reloading"
 
-systemctl enable catalogue
+systemctl enable catalogue &>> $LOGFILE
 
 VALIDATE $? "Enabling catalogue application"
 
-systemctl start catalogue
+systemctl start catalogue &>> $LOGFILE
 
 VALIDATE $? "Starting catalogue application"
 
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "Copying mongo repo"
 
-dnf install mongodb-org-shell -y
+dnf install mongodb-org-shell -y &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB Client"
 
-mongo --host $MongoDB_Host </app/schema/catalogue.js
+mongo --host $MongoDB_Host </app/schema/catalogue.js &>> $LOGFILE
 
 VALIDATE $? "Loading catalogue data into Mongo DB"
 
